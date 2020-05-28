8820a63b58b6b999d218da5d6b19070ced2c603b
Peter Gasparik <gasparik.pe@gmail.com>
2020-05-28 20:44:08 +0200

    build and deploy

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,7 @@ export { Closable } from './observable/Closable';
 export { Observable } from './observable/Observable';
 export { Dispatchable } from './observable/Dispatchable';
 export { Listener } from './observable/Listener';
+export { ObservableAdapter } from './adapter/ObservableAdapter';
 export { Channel } from './channel/Channel';
 export { ProxyChannel } from './channel/ProxyChannel';
 export { StatefulObservable } from './observable/StatefulObservable';
